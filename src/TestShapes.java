2a8e22fb66c4af0e912d7c83d01ada216013e605
oguzhansukrukeles <oguzhansukrukeles@posta.mu.edu.tr>
2025-04-18 12:18:33 +0300

    week8

---
+++ src/TestShapes.java
@@ -0,0 +1,7 @@
+public class TestShapes {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle("blue",3,4);
+        Rectangle r2 = new Rectangle("blue",3,4);
+        System.out.println(r1.equals(r2));
+    }
+}
